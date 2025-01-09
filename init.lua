978eb3678953856bf05ea7dc6d85c6118889d3a4
Harrison Miller <harrison.miller@trillianthealth.com>
2025-01-09 07:37:00 -0600

    added mini.starter

--- init.lua
+++ init.lua
@@ -931,6 +931,8 @@ require('lazy').setup({
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
+      require('mini.starter').setup { use_icons = vim.g.have_nerd_font }
+
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
